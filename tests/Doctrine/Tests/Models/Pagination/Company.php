bdb66f505749079d39a7b0ca48a9d39443fcd616
vaheshadunts <v.shadunts@gmail.com>
2015-03-27 04:26:53 -0800

    Testcase for PR1325 and PR1337

---
+++ tests/Doctrine/Tests/Models/Pagination/Company.php
@@ -0,0 +1,35 @@
+<?php
+namespace Doctrine\Tests\Models\Pagination;
+
+/**
+ * Company
+ *
+ * @package Doctrine\Tests\Models\Pagination
+ *
+ * @author Bill Schaller
+ * @Entity
+ * @Table(name="pagination_company")
+ */
+class Company
+{
+    /**
+     * @Id @Column(type="integer")
+     * @GeneratedValue
+     */
+    public $id;
+
+    /**
+     * @Column(type="string")
+     */
+    public $name;
+
+    /**
+     * @OneToOne(targetEntity="Logo", mappedBy="company", cascade={"persist"}, orphanRemoval=true)
+     */
+    public $logo;
+
+    /**
+     * @OneToMany(targetEntity="Department", mappedBy="company", cascade={"persist"}, orphanRemoval=true)
+     */
+    public $departments;
+}
